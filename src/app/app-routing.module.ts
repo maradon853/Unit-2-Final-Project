40db9af09586f1fa4eb95345ba547940885fd22e
mary <maradon853@gmail.com>
2020-04-02 20:24:01 -0500

    started some routing and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OuterSpaceComponent } from './outer-space/outer-space.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { RullingPlanetComponent } from './rulling-planet/rulling-planet.component';
+import { AppComponent } from './app.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: "", component: AppComponent },
+  { path: "planets", component: PlanetsComponent },
+  { path: "rulling planet", component: RullingPlanetComponent },
+  { path: "apod", component: OuterSpaceComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
