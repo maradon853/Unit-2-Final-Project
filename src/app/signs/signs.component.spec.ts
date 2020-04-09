676b79dae92746f3a043ec14ec18895294cd4c7b
mary <maradon853@gmail.com>
2020-04-09 17:21:43 -0500

    more tests

--- src/app/signs/signs.component.spec.ts
+++ src/app/signs/signs.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SignsComponent } from './signs.component';
 
 describe('SignsComponent', () => {
@@ -9,7 +8,7 @@ describe('SignsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SignsComponent ]
-      
+  
     })
     .compileComponents();
   }));
@@ -22,5 +21,16 @@ describe('SignsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should display a list of signs`, () => {
+    expect(component.signs.length).toEqual(12);
+  });
+
+  it('should set sign spans to light blue', () => {
+    //const fixture = TestBed.createComponent(AppComponent);
+    //fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('li').backgroundcolor).toBe('lightblue');
   });
 });
